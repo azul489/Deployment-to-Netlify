6a847328de73cf90f0931943445313612c3d08a8
azul fe12 <azultaufik104@gmail.com>
2022-12-18 12:14:52 +0700

    Deployment to Netlify

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Ahmad Zulkifli Taufik" // TODO: replace this
+const studentId = "FE2688063" // TODO: replace this
+
+export { url, studentName, studentId }
